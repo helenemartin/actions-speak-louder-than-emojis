f3c1d1dc98222477d68709307b59303d315ac071
Your Name <helenemartin13@hotmail.com>
2020-10-05 15:11:41 +0100

    Set up index, create Router and switch function in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-const App = () => {
-    return <div>App</div>;
-};
+export default function App() {
+    return (
+        <Router>  
+            <switch>
+                <Route path="/campains">
+                    <p> I am at the campains page</p>
+                </Route>
+                <Route path="/signin">
+                    <p> I am at the signin page</p>
+                </Route> 
 
-export default App;
+                <Route path="/fqa">
+                    <p> I am at the fqa page</p>
+                </Route> 
+                <Route path="/about">
+                    <p> I am at the about page</p>
+                </Route> 
+
+                <Route path="/"> 
+                    <p> I am at the home page</p>
+                </Route>
+            </switch>  
+        </Router>
+    );
+}
