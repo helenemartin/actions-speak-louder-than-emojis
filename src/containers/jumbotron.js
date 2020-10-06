0e206731143725484dd5aba8e719e004a2451101
Your Name <helenemartin13@hotmail.com>
2020-10-06 13:57:40 +0100

    fix the Pane tag to wrap text and image

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -10,6 +10,8 @@ export function JumbotronContainer() {
                     <Jumbotron.Pane>
                         <Jumbotron.Title>{item.title}</Jumbotron.Title>
                         <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+                    </Jumbotron.Pane>
+                    <Jumbotron.Pane>
                         <Jumbotron.Image src={item.image} alt={item.alt} />
                     </Jumbotron.Pane>
                 </Jumbotron>
