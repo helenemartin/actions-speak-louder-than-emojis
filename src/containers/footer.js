07747bcbc89717afd0cad337cb5b05c47e4c193d
Your Name <helenemartin13@hotmail.com>
2020-10-06 14:59:10 +0100

    add all other links

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -8,6 +8,20 @@ export function FooterContainer() {
             <Footer.Row>
                 <Footer.Column>
                     <Footer.Link href="#">FAQs</Footer.Link>
+                    <Footer.Link href="#">About</Footer.Link>
+                    <Footer.Link href="#">Contact</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="#">How it works</Footer.Link>
+                    <Footer.Link href="#">Campains</Footer.Link>
+                    <Footer.Link href="#">Sponsors</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link href="#">join</Footer.Link>
+                    <Footer.Link href="#">Sign-in</Footer.Link>
+                    <Footer.Link href="#">Sign-up</Footer.Link>
                 </Footer.Column>
             </Footer.Row>
         </Footer>
