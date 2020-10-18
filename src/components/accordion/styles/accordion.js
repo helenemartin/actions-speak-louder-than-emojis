e9a71156946e16939b6c9f92c7cab4811f2b175a
Your Name <helenemartin13@hotmail.com>
2020-10-18 16:55:36 +0100

    populate the faqs container with json data

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,8 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.section``;
 
 export const Inner = styled.div``;
+
+export const Item = styled.div``;
 
 export const Title = styled.h1``;
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDom from 'react-dom'
-import { GlobalStyles } from './global-styles'
-import App from './app';
-
+import React from "react";
+import ReactDom from "react-dom";
+import { GlobalStyles } from "./global-styles";
+import App from "./app";
 
 ReactDom.render(
-    <>
-        <GlobalStyles />
-        <App />
-    </>, 
-    document.getElementById('root')
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
 );
