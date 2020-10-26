917246dd84a80e2288c8fc496d0d428e6fee7165
Your Name <helenemartin13@hotmail.com>
2020-10-26 23:08:35 +0000

    complete the whole card scaffold with styling

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { Container } from "./styles/card";
+import { Container, Body, Title, Text, Image, Button } from "./styles/card";
 
 export default function Card({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+}
+
+export default function Body({ children, ...restProps }) {
+  return <Body {...restProps}>{children}</Body>;
+}
+
+export default function Title({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+}
+
+export default function Text({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+}
+
+export default function Image({ children, ...restProps }) {
+  return <Image {...restProps}>{children}</Image>;
+}
+export default function Button({ children, ...restProps }) {
+  return <Button {...restProps}>{children}</Button>;
 }
--- src/containers/card.js
+++ src/containers/card.js
@@ -3,8 +3,12 @@ import { Card } from "../components";
 
 export function CardContainer() {
   return (
-    <Card>
-      <dv>Hello</dv>
-    </Card>
+      {movies.map(movie => (
+        <Card>
+            Card.Image src={movie.image} alt={movie.title) />
+            </Card>
+        
+    ))}
+     
   );
 }
