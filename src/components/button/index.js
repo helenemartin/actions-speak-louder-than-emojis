eb60ff2228b7641453d561e207c0a924b02bec0b
Your Name <helenemartin13@hotmail.com>
2020-11-02 14:18:42 +0000

    add a wrapper to the buttons

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,14 +1,7 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const StyledButton = styled.button`
-  padding: 1 rem 1.5rem;
-  font-size: 1.8rem;
-  color: $(({primary})props => (primary ? 'red' : '#fff');
-  outline: none;
-  border: none;
-  bacground-color: #333;
+export const wrapper = styled.section`
+  margin-top: 2rem;
+  background-color: paleturquoise;
 `;
-export const Button = {{ primary, children }} => {
-    return <StyledButton primary = {primary}>{children}</Syledbutton>;
-};
--- src/components/button/styles/button.js
+++ src/components/button/styles/button.js
@@ -1,1 +1,29 @@
-import styled from "styled-components";
+import styled, {css} from 'styled-components';
+
+import {wrapper} from './styles/button'
+
+export const StyledButton = styled.button`
+  background-color: white;
+  color: palevioletred;
+  font-size: 1.2rem;
+  margin: 1rem;
+  padding: 0.5 rem 1rem;
+  border: 1px solid palevioletred;
+  border-radius: 3px;
+
+  ${({ primary }) => 
+  primary && css`
+  background-color: palevioletred;
+  color: white;
+  box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.2);
+    `}
+`;
+
+export const SuperButton = styled(StyledButton)`
+    font-size: 2.5rem;
+`;
+
+export const Button = {{ primary, children }} => {
+    return <StyledButton primary = {primary}>{children}</Syledbutton>;
+};
+
