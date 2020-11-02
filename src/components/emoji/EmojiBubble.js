c2ca66c858b7e4eb9ff9b4917ed9ad556d233ab1
Your Name <helenemartin13@hotmail.com>
2020-11-02 22:22:04 +0000

    wrap up the EmojiBubble component

--- src/components/emoji/EmojiBubble.js
+++ src/components/emoji/EmojiBubble.js
@@ -4,7 +4,7 @@ import EmojiWrapper from "./EmojiWrapper";
 const floatAnimation = (one, two) => keyframes `
 0% { bottom: -10%; transform : translateX(0)}
 50% { transform: translateX(${one}px)}
-100% { transform: translateX(${two}px)}
+100% { bottom: 110% ;transform: translateX(${two}px)}
 `;
 
 const EmojiBubble = styled(EmojiWrapper)`
@@ -12,5 +12,8 @@ const EmojiBubble = styled(EmojiWrapper)`
   bottom: 0;
   left: ${({ left }) => (left ? left : 10%;
     font-size: ${({ size }) => size ? size  : 2 }rem;
-    animation: 
+    animation: ${({one, two}) => floatAnimation(one, two)} 
+    $ {({ size }) => size < 3 ? 5 : 6}s ease-in forwards;
 `;
+
+export default EmojiBubble;
