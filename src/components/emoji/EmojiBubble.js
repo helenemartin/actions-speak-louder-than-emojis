bdfa2cfee39a1c3313baa6fea2dabc44017aa7d5
Your Name <helenemartin13@hotmail.com>
2020-11-02 22:16:08 +0000

    implement a keyframe animation styled component

---
+++ src/components/emoji/EmojiBubble.js
@@ -0,0 +1,16 @@
+import styled, { keyframes } from "styled-components";
+import EmojiWrapper from "./EmojiWrapper";
+
+const floatAnimation = (one, two) => keyframes `
+0% { bottom: -10%; transform : translateX(0)}
+50% { transform: translateX(${one}px)}
+100% { transform: translateX(${two}px)}
+`;
+
+const EmojiBubble = styled(EmojiWrapper)`
+  position: absolute;
+  bottom: 0;
+  left: ${({ left }) => (left ? left : 10%;
+    font-size: ${({ size }) => size ? size  : 2 }rem;
+    animation: 
+`;
