59ce803304542b559548231a0a4dda7c24198229
Your Name <helenemartin13@hotmail.com>
2020-11-15 19:39:46 +0000

    build skeleton for the browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Header } from "../components";
+import * as ROUTES from "../constants/routes";
+import { FirebaseContext } from "../context/firebase";
+import { SelectProfileContainer } from "./profiles";
+import { FooterContainer } from "./footer";
 
 export function BrowseContainer() {
-  return <p>Browse Container</p>;
+  const [profile, setProfile] = useState({});
+  const user = {
+    displayName: "LN",
+    photoURL: "1"
+  };
+  return profile.displayName ? (
+    <>
+      <p>Browse Container</p>
+      <FooterContainer />
+    </>
+  ) : (
+    <SelectProfileContainer />
+  );
 }
