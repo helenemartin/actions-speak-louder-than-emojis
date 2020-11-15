ab431f1b764eb7fe75b7ac82244fcb1936a4fec1
Your Name <helenemartin13@hotmail.com>
2020-11-15 20:16:34 +0000

    style profiles component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Header } from "../components";
 import * as ROUTES from "../constants/routes";
 import { FirebaseContext } from "../context/firebase";
 import { SelectProfileContainer } from "./profiles";
