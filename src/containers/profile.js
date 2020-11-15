924a21862c2aa7f34f222c2ece87f69c495c62ed
Your Name <helenemartin13@hotmail.com>
2020-11-15 19:50:03 +0000

    architecture profiles and the component profiles

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { Header, Profiles } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export function SelectProfileContainer() {
-  return <p>Profile container</p>;
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo
+            to={ROUTES.HOME}
+            src="/images/portrait.png"
+            alt="Helene"
+          />
+        </Header.Frame>
+      </Header>
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL
+              })
+            }
+          >
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    </>
+  );
 }
