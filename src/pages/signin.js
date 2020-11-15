253afe067e44025ea7ce2f793c97276e8a8cc7f9
Your Name <helenemartin13@hotmail.com>
2020-11-15 19:02:14 +0000

    architecture the signup page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form } from "../components";
 import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
 
 export default function Signin() {
   const [error, setError] = useState("");
@@ -16,37 +17,40 @@ export default function Signin() {
   };
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-        <Form.Base onSubmit={handleSignin} method="POST">
-          <Form.Input
-            placeholder="Email address"
-            value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <Form.Input
-            type="password"
-            value={password}
-            autoComplete="off"
-            placeholder="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <Form.Submit disabled={isInvalid} type="submit">
-            Sign In
-          </Form.Submit>
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
 
-          <Form.Text>
-            Ready for meaningful actions?{" "}
-            <Form.Link to="/signup">Sign up now.</Form.Link>
-          </Form.Text>
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA.
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+            <Form.Text>
+              Ready for meaningful actions?{" "}
+              <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
---
+++ src/pages/signup.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
+
+export default function Signup() {
+  return (
+    <HeaderContainer>
+      <p>Hello</p>
+    </HeaderContainer>
+  );
+}
