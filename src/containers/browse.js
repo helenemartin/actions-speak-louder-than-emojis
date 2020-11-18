a8503f7f5fa25e66c908dd2f6e4217d830bfaef2
Your Name <helenemartin13@hotmail.com>
2020-11-18 16:37:46 +0000

    implement the card logic in the card compound component

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -86,12 +86,14 @@ export function BrowseContainer({ slides }) {
 
       <Card.Group>
         {slideRows.map(slideItem => (
-          <Card key={}>
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
             <Card.Title>{slideItem.title}</Card.Title>
             <Card.Entities>
               {slideItem.data.map(item => (
                 <Card.Item key={item.docId} item={item}>
-                  <Card.Image src={} />
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
                   <Card.Meta>
                     <Card.SubTitle></Card.SubTitle>
                     <Card.Text></Card.Text>
