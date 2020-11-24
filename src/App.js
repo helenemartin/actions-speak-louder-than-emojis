3adc9326a535cff920569b5be6dc386bd28546ec
Your Name <helenemartin13@hotmail.com>
2020-11-24 13:52:56 +0000

    implement custom auth listener hook

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { useAuthListener } from "./hooks";
 
 export function App() {
+  const { user } = useAuthListener();
   return (
     <Router>
       <Switch>
